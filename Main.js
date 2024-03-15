11ce6b99270951cd10f214b7b222a1e1ac3e4c22
Omagadness <163418670+Omagadness@users.noreply.github.com>
2024-03-15 10:06:48 -0300

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+  document.getElementById("imagem").src ="https://boxloja-std-assets.nyc3.cdn.digitaloceanspaces.com/lojas/d4nxd/produtos/2845ce27-9b1a-4b68-8d07-b769d8fff754.jpg"
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://noticias.animeonegai.com/wp-content/uploads/2022/06/dragon-ball-Yamcha-muerte-1024x576.webp"
+}
